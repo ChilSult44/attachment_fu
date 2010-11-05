e22a63fec3e637539d9f4151e9c89162ffac25e0
Ian Enders <ian.enders@gmail.com>
2010-11-05 19:02:53 -0400

    image attachments

---
+++ lib/intersect/image_attachments.rb
@@ -0,0 +1,33 @@
+module Intersect::ImageAttachments
+  
+  def self.included(included_into)
+    
+    included_into.module_eval do
+      has_many :images, :class_name => "#{self.name}Image"
+      after_save :save_attachments
+
+      def attachable_replace=(param)
+        @attachment_replace = param
+      end
+
+      def attachable=(params)
+        @attachments = params.keys.sort.inject([]) { |vals, k| vals.push(params[k]) }.reject {|a| a[:uploaded_data].blank? }.collect { |a| "#{self.class.name}Image".constantize.new(a) }
+      end
+
+      protected
+      def save_attachments
+        if @attachment_replace
+          images.destroy_all
+        end
+        
+        if @attachments
+          @attachments.each do |attachment|
+            images << attachment
+          end
+        end
+      end
+      
+    end
+  end
+  
+end
